4069704b5757d6190b40fb5f1b5238cb415511d5
K0R0STYL3V <255818@astanait.edu.kz>
2026-01-05 10:37:46 +0500

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,15 +1,17 @@
-public class Guest {
-    private String name;
-
-    public Guest(String name) {
-        this.name = name;
+public class Guest extends Person {
+    private int reservationId;
+    public Guest(String name, int reservationId) {
+        super(name);
+        setReservationId(reservationId);
     }
-
-    public String getName() {
-        return name;
+    @Override
+    public void printDetails() {
+        System.out.println("Guest: " + getName() + " (Booking ID: " + reservationId + ")");
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public int getReservationId() {
+        return reservationId;
+    }
+    public void setReservationId(int reservationId){
+        this.reservationId = reservationId;
     }
 }
